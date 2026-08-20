5519bf3fe0a8d2646c17b6c044cc6ce4432784ce
RahulKolpe03 <rahul.kolpe.cmaug26@gmail.com>
2026-08-20 16:22:52 +0530

    Add files via upload

---
+++ Assignment  01/checkPositive.java
@@ -0,0 +1,13 @@
+public class checkPositive{
+
+          public static void main(String args[]){
+                                     int num = 3;
+if (num >= 0 ){
+System.out.println("Number is positive") ;
+
+} else {
+System.out.println("Number is negative ");
+}
+
+}                                         
+}
