5581d9359292f0172fc537959e3f28009229a316
RahulKolpe03 <rahul.kolpe.cmaug26@gmail.com>
2026-08-20 16:35:32 +0530

    Add files via upload

---
+++ Assignment 02 - code/Variables.java
@@ -0,0 +1,23 @@
+class Variables
+{
+    public static void main(String args[])
+    {
+        byte a = 10;
+        short b = 20;
+        int c = 30;
+        long d = 40;
+        float e = 50.5f;
+        double f = 60.5;
+        char g = 'A';
+        boolean h = true;
+
+        System.out.println("byte value = " + a);
+        System.out.println("short value = " + b);
+        System.out.println("int value = " + c);
+        System.out.println("long value = " + d);
+        System.out.println("float value = " + e);
+        System.out.println("double value = " + f);
+        System.out.println("char value = " + g);
+        System.out.println("boolean value = " + h);
+    }
+}
